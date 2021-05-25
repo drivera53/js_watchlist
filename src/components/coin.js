835395ee3ab74b4180e09da514859e00e0a06d8f
Danny Rivera <samboard@hotmail.com>
2021-05-25 11:51:37 -0400

    View Coin functionality added.

--- src/components/coin.js
+++ src/components/coin.js
@@ -1,18 +1,29 @@
 class Coin {
     static all = []
 
-    constructor({id}, watchlist_id) {
+    constructor({id}, watchlist_id, {coin_api_id, name, symbol, image, current_price, price_change_percentage_1h_in_currency, high_24h, low_24h, total_volume, market_cap, market_cap_rank, circulating_supply}) {
         this.id = id
         this.watchlist_id = watchlist_id
+        this.coin_api_id = coin_api_id
+        this.name = name
+        this.symbol = symbol
+        this.image = image
+        this.current_price = current_price
+        this.price_change_percentage_1h_in_currency = price_change_percentage_1h_in_currency
+        this.high_24h = high_24h
+        this.low_24h = low_24h
+        this.total_volume = total_volume
+        this.market_cap = market_cap
+        this.market_cap_rank = market_cap_rank
+        this.circulating_supply = circulating_supply
         Coin.all.push(this)    
     }
 
-    render() {
-        return(`<div class="box" id="coin-${this.id}" data-id=${this.id}>
+    render() { 
+        return(`<div class="coin_box" id="coin-${this.id}" data-id=${this.id}>
                 <h2>Coin ID: ${this.id}</h2>
+                <h2>Coin ID: ${this.name}</h2>
                 <p>Watchlist ID: ${this.watchlist_id}</p>
-                <button data-action='view'>View</button>
-                <button data-action='edit'>Edit</button> 
                 <button data-action='delete'>Delete</button>
             </div>`
         )
--- src/components/flatcoinCoinAdapter.js
+++ src/components/flatcoinCoinAdapter.js
@@ -4,7 +4,7 @@ class FlatcoinCoinAdapter {
         this.baseURL = baseURL
     }
 
-    // Fetch Watchlist Function
+    // Testing new 
     getCoinlist(div) {
         fetch(this.baseURL + `watchlists/` + div.dataset.id)
         .then(resp => {
@@ -14,22 +14,34 @@ class FlatcoinCoinAdapter {
         .then(data => {
             console.log(data)
             data.forEach(coin => {
-                // console.log(watchlist)
-                const c = new Coin(coin, div.dataset.id)
-                //console.log(w)
-                c.addToDom()
+                fetch(`http://127.0.0.1:3000/coins/${coin.id}`)
+                .then(resp => {
+                console.log(resp)
+                return resp.json()
+                })
+                .then(updated_coin_data => {
+                    const c = new Coin(coin, div.dataset.id, updated_coin_data)
+                    c.addToDom()
+                })
             })
-        })
-        // .then(res => res.json())
-        // .then(watchlists => {
-        //     watchlists.forEach(watchlist => {
-        //         // console.log(watchlist)
-        //         const w = new Watchlist(watchlist)
-        //         //console.log(w)
-        //         w.addToDom()
-        //     })
-        // })    
+        })   
     }
 
-
+        // // Fetch Watchlist Function
+    // getCoinlist(div) {
+    //     fetch(this.baseURL + `watchlists/` + div.dataset.id)
+    //     .then(resp => {
+    //         console.log(resp)
+    //         return resp.json()
+    //     })
+    //     .then(data => {
+    //         console.log(data)
+    //         data.forEach(coin => {
+    //             // console.log(watchlist)
+    //             const c = new Coin(coin, div.dataset.id)
+    //             //console.log(w)
+    //             c.addToDom()
+    //         })
+    //     })   
+    // }
 }
