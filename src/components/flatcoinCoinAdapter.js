7a44ba25e14f2d11bb2050090f40837d78584d7b
Danny Rivera <samboard@hotmail.com>
2021-05-27 08:11:56 -0400

    Fixed a bug that prevented deleting a coin that I just created.

--- src/components/flatcoinCoinAdapter.js
+++ src/components/flatcoinCoinAdapter.js
@@ -54,7 +54,7 @@ class FlatcoinCoinAdapter {
                 return resp.json()
                 })
                 .then(updated_coin_data => {
-                    const c = new Coin(watchlist_id, updated_coin_data)
+                    const c = new Coin(watchlist_id.value, updated_coin_data)
                     c.addToDom()
                     confirmationCoinCreateContainer.style.display = "block"           
                     setTimeout(function(){confirmationCoinCreateContainer.style.display = "none"}, 1500)
